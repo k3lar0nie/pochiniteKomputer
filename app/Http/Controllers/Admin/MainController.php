de10cd1aa8108fb881c5ebc507b35de137ee2beb
student5 <studkab5@jdk.ru>
2026-04-21 10:58:51 +0500

    1 praktiko

---
+++ app/Http/Controllers/Admin/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class MainController extends Controller
+{
+    public function index() {
+        return view('admin.index');
+        //sadgfasiodghilasdlkgkjhlasdlhgljhasdljhlj КАКОЙ АККАУНТ СЕМЕНА АЛО ЭТО Я ЭТО НЕ СЕМЕН
+    }
+}
