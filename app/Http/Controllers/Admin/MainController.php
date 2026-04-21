66a3d73da055b6acd1d8f8c480f5277be6ec0ec0
k3lar0nie <k3lar0nie@yahoo.com>
2026-04-21 11:00:51 +0500

    11

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -9,5 +9,7 @@ class MainController extends Controller
     public function index() {
         return view('admin.index');
         //sadgfasiodghilasdlkgkjhlasdlhgljhasdljhlj КАКОЙ АККАУНТ СЕМЕНА АЛО ЭТО Я ЭТО НЕ СЕМЕН
+        //sadgfasiodghilasdlkgkjhlasdlhgljhasdljhlj КАКОЙ АККАУНТ СЕМЕНА АЛО ЭТО Я ЭТО НЕ СЕМЕН
+
     }
 }
